a9fd2bd6eb9fe70738a720ac2902b0acbf3a4ffe
autouav <autouav@gmail.com>
2015-07-05 11:33:59 +0300

    first commit

---
+++ autoUAV/src/es/pymasde/blueterm/getNavData.java
@@ -0,0 +1,42 @@
+package es.pymasde.blueterm;
+
+import com.codeminders.ardrone.ARDrone;
+import com.codeminders.ardrone.NavData;
+import com.codeminders.ardrone.NavDataListener;
+
+public class getNavData {
+    ARDrone drone;
+    String str[];
+    NavDataListener ndl;
+    public float Battery;
+    public float Yaw;
+    public float Roll;
+    public float Pitch;
+    public float Altitude;
+
+
+    public getNavData(ARDrone drone, String str[]) {
+        this.drone = drone;
+        this.str = str;
+        Battery = Yaw = Roll = Pitch = Altitude = -1;
+    }
+
+    public void getNavdata() {
+        ndl = new NavDataListener() {
+            @Override
+            public void navDataReceived(NavData nd) {
+                String temp = "";
+                Battery = nd.getBattery();
+                Yaw = nd.getYaw();
+                if (Yaw <0 && Yaw!=-1) Yaw += 360;
+                Roll = nd.getRoll();
+                Pitch = nd.getPitch();
+                Altitude = nd.getAltitude();
+                temp = temp + "Battery: " + Battery +"\t\t\t"+  "Yaw:         " + Yaw + "\n"+ "Roll:        " + Roll + "\t\t\t" + "Pitch:       " + Pitch + "\n";
+                temp = temp + "Altitude:    " + Altitude + "\t\t\t" + "flyingState: " + nd.getFlyingState() + "\n";
+                str[0] = temp;
+            }
+        };
+        drone.addNavDataListener(ndl);
+    }
+}
